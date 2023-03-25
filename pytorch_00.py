abcf21e77ab0b168d5186261a2a993590bc47c53
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2023-03-25 16:49:03 +0600

    getting some info from the tensor

--- pytorch_00.py
+++ pytorch_00.py
@@ -11,10 +11,19 @@ import matplotlib.pyplot as plt
 
 # print(random_tensor)
 
-float_32_tensor = torch.tensor(
-    [3.0, 6.0, 9.0], dtype=None, device=None, requires_grad=False
-)
+# float_32_tensor = torch.tensor(
+#     [3.0, 6.0, 9.0], dtype=None, device=None, requires_grad=False
+# )
 
-int_32_tensor = torch.tensor([3, 5, 7], dtype=torch.long)
+# int_32_tensor = torch.tensor([3, 5, 7], dtype=torch.long)
 
-print(int_32_tensor * float_32_tensor)
+# print(int_32_tensor * float_32_tensor)
+
+## Getting info from tensors..
+
+some_tensor = torch.rand(3, 4)
+
+print(f"Datatype of the tensor: {some_tensor.dtype}")
+print(f"Shape of tensor: {some_tensor.shape}")
+print(f"Size of tensor: {some_tensor.size()}")
+print(f"Device of tensor: {some_tensor.device}")

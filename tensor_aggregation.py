1ddd7375cbb0750391f2903dd84d2f86806a5f32
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2023-03-26 05:36:48 +0600

    update

--- tensor_aggregation.py
+++ tensor_aggregation.py
@@ -8,12 +8,16 @@ import matplotlib.pyplot as plt
 
 x = torch.arange(0, 100, 10)
 
-print(x)
-print(x.min())
-print(x.dtype)
+# print(x)
+# print(x.min())
+# print(x.dtype)
 
-## torch.mean() functions requires a tensor of dtype float32 to work properly
-print(torch.mean(x.type(torch.float32)))
-print(x.type(torch.float32).mean())
+# ## torch.mean() functions requires a tensor of dtype float32 to work properly
+# print(torch.mean(x.type(torch.float32)))
+# print(x.type(torch.float32).mean())
 
-print(x.sum())
+# print(x.sum())
+
+## Finding positional aggregation
+print(x.argmax())
+print(x[x.argmax()])

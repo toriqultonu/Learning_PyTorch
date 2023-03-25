f418fb5aed5bde1851d8b89850620573156f6d25
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2023-03-26 04:42:24 +0600

    aggregation done

---
+++ tensor_aggregation.py
@@ -0,0 +1,19 @@
+import torch
+import torch
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# create a tensor
+
+x = torch.arange(0, 100, 10)
+
+print(x)
+print(x.min())
+print(x.dtype)
+
+## torch.mean() functions requires a tensor of dtype float32 to work properly
+print(torch.mean(x.type(torch.float32)))
+print(x.type(torch.float32).mean())
+
+print(x.sum())

07c88266fd9ec62175afd0e5a2f063e91b896460
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2023-03-25 16:43:23 +0600

    update

--- pytorch_00.py
+++ pytorch_00.py
@@ -15,6 +15,6 @@ float_32_tensor = torch.tensor(
     [3.0, 6.0, 9.0], dtype=None, device=None, requires_grad=False
 )
 
-int_32_tensor = torch.tensor([3, 5, 7], dtype=torch.int32)
+int_32_tensor = torch.tensor([3, 5, 7], dtype=torch.long)
 
 print(int_32_tensor * float_32_tensor)

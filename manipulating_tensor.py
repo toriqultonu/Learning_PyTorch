52ac47c0c29b2ed39d33f1fad8568252cc4dfb5f
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2023-03-25 16:57:24 +0600

    some operation done

--- manipulating_tensor.py
+++ manipulating_tensor.py
@@ -4,3 +4,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+tensor = torch.tensor([1, 2, 3])
+
+print(tensor + 10)
+
+print(tensor * 10)
+
+# pytorch in-build functions:
+tensor = torch.add(tensor, 10)
+tensor = torch.mul(tensor, 30)
+print(tensor)

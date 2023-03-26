6deee24ba784ef875d9b3d7a361a048642443fc3
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2023-03-27 05:25:08 +0600

    running tensor on gpu

--- running_tensors_on_gpu.py
+++ running_tensors_on_gpu.py
@@ -5,11 +5,25 @@ import matplotlib.pyplot as plt
 
 ## Getting a gpu
 
-print(torch.cuda.is_available())
+# print(torch.cuda.is_available())
 
 ## setup device agonostic code
 device = "cuda" if torch.cuda.is_available() else "cpu"
-print(device)
+# print(device)
 
-## count number of devices
-print(torch.cuda.device_count())
+# ## count number of devices
+# print(torch.cuda.device_count())
+
+## puting tensor models on gpu
+
+tensor = torch.tensor([1, 2, 3], device="cpu")
+
+print(tensor, tensor.device)
+
+tensor_on_gpu = tensor.to(device)
+print(tensor_on_gpu, tensor_on_gpu.device)
+
+## moving tensor back to cpu
+
+tensor_on_cpu = tensor_on_gpu.cpu().numpy()
+print(tensor_on_cpu)

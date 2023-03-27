88192fc31e19c24e0671a0c33adbf97bc1fe9c4d
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2023-03-27 12:14:32 +0600

    data visualized

--- PyTorch Work Flow/regression_formula.py
+++ PyTorch Work Flow/regression_formula.py
@@ -24,4 +24,30 @@ X_train, y_train = X[:train_split], y[:train_split]
 
 X_test, y_test = X[train_split:], y[train_split:]
 
-print(len(X_train), len(y_train), len(X_test), len(y_test))
+# print(len(X_train), len(y_train), len(X_test), len(y_test))
+
+
+def plot_predictions(
+    train_data=X_train,
+    train_label=y_train,
+    test_data=X_test,
+    test_label=y_test,
+    predictions=None,
+):
+    plt.figure(figsize=(10, 7))
+
+    # plot training data in blue
+    plt.scatter(train_data, train_label, c="b", s=4, label="Training Data")
+
+    # plot testing data in green
+    plt.scatter(test_data, test_label, c="g", s=4, label="Testing Data")
+
+    if predictions is not None:
+        plt.scatter(test_data, predictions, c="r", s=4, label="Predictions")
+
+    # showing the plt
+    plt.legend(prop={"size": 14})
+    plt.show()
+
+
+plot_predictions()

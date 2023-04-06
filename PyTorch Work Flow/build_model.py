45a40d094f6dafc967e2954d0591121145e46ea4
toriqultonu <50527978+toriqultonu@users.noreply.github.com>
2023-04-06 11:08:18 +0600

    new start

--- PyTorch Work Flow/build_model.py
+++ PyTorch Work Flow/build_model.py
@@ -4,14 +4,21 @@ import matplotlib.pyplot as plt
 
 ## creating a linear regression model class
 
+
 class LinearRegressionModel(nn.Module):
-    
     def __init__(self):
         super().__init__()
 
-        self.weight = nn.Parameter(torch.randn(1, requires_grad=True, dtype=torch.float))
+        self.weight = nn.Parameter(
+            torch.randn(1, requires_grad=True, dtype=torch.float)
+        )
         self.bias = nn.Parameter(torch.randn(1, requires_grad=True, dtype=torch.float))
 
         # forward method to define the computation in the model
         def forward(self, x: torch.Tensor) -> torch.Tensor:
             return self.weight * x + self.bias
+    
+
+model_0 = LinearRegressionModel()
+
+print(model_0.state_dict())

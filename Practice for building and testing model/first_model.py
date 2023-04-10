f17390da7c87c8ebda061c87d52b97db8e2b28fd
toriqultonu <50527978+toriqultonu@users.noreply.github.com>
2023-04-10 15:03:50 +0600

    layer implemented

--- Practice for building and testing model/first_model.py
+++ Practice for building and testing model/first_model.py
@@ -3,11 +3,11 @@ from torch import nn
 import matplotlib.pyplot as plt
 
 ## check pytorch version
-print(torch.__version__)
+# print(torch.__version__)
 
 # Create device agnostic code
 device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"Using device: {device}")
+# print(f"Using device: {device}")
 
 ## Create some data using the linear regression formula of y = weight * X + bias
 weight = 0.7
@@ -32,7 +32,7 @@ train_split = int(0.8 * len(X))
 X_train, y_train = X[:train_split], y[:train_split]
 X_test, y_test = X[train_split:], y[train_split:]
 
-print(len(X_train), len(y_train), len(X_test), len(y_test))
+# print(len(X_train), len(y_train), len(X_test), len(y_test))
 
 
 # Create plot prediction function
@@ -51,14 +51,26 @@ def plot_predictions(
     # plot testing data in green
     plt.scatter(test_data, test_label, c="g", s=4, label="Testing Data")
 
-    if predictions is not None:
-        plt.scatter(test_data, predictions, c="r", s=4, label="Predictions")
 
-    # showing the plt
-    plt.legend(prop={"size": 14})
-    plt.show()
+## Building a pytorch linear model
 
+
+# Create a linear model by subclassing nn.Module
+class LinearRegressionModelV2(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # Use nn.Linear() for createing the model parameters.
+        self.linear_layer = nn.Linear(in_features=1, out_features=1)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.linear_layer(x)
+
+
+# Set the manual seed
+torch.manual_seed(42)
+model_1 = LinearRegressionModelV2()
+print(model_1, model_1.state_dict())
 
 # Using plot prediction
-
-plot_predictions(X_train, y_train, X_test, y_test)
+# plot_predictions(X_train, y_train, X_test, y_test)

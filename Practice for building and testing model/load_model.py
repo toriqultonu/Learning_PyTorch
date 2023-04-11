fe94604b97ae7a929e4bc5948f3757abc3901bda
toriqultonu <50527978+toriqultonu@users.noreply.github.com>
2023-04-11 11:34:09 +0600

    loading model done

---
+++ Practice for building and testing model/load_model.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+import torch
+from torch import nn
+import matplotlib.pyplot as plt
+import first_model
+import save_model
+
+## Loading model
+
+# Create a new instance of the model
+loaded_model_1 = first_model.LinearRegressionModelV2()
+
+# Load the saved model_1 sate dict
+loaded_model_1.load_state_dict(torch.load(save_model.MODEL_SAVE_PATH))
+
+# Put the loaded model to device
+loaded_model_1.to(device=first_model.device)
+
+print(loaded_model_1)
+print(loaded_model_1.state_dict())

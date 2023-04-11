759b1631ade90e4c30ae75adcd001722ad75adb3
toriqultonu <50527978+toriqultonu@users.noreply.github.com>
2023-04-12 00:05:46 +0600

    creating random circle data set

---
+++ Pytorch Classification/neural_network_classification.py
@@ -0,0 +1,19 @@
+import torch
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn.datasets import make_circles
+
+## Make classification data and get it ready
+
+# Make 1000 samples
+n_samples = 1000
+
+# Create circles
+X, y = make_circles(n_samples=n_samples, noise=0.03, random_state=42)
+
+print(len(X), len(y))
+print(f"First 5 sample of X: {X[:5]}")
+print(f"First 5 sample of y: {y[:5]}")
+print(y)

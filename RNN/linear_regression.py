dd9c5adc2d97f6b84157cbba2aa7149035296ee0
toriqultonu <50527978+toriqultonu@users.noreply.github.com>
2023-05-28 08:27:09 +0600

    test done

--- RNN/linear_regression.py
+++ RNN/linear_regression.py
@@ -1,31 +1,31 @@
-
 import numpy as np
 
 
 class LinearRegression:
-
-    def __init__(self, lr = 0.001, n_iters = 1000):
-        self.lr = lr
+    def __init__(self, learning_rate=0.0001, n_iters=1000):
+        self.lr = learning_rate
         self.n_iters = n_iters
-        self.weight = None
+        self.weights = None
         self.bias = None
 
     def fit(self, X, y):
-        # initialize parameters
-        n_samples, n_feature = X.shape
-        self.weights = np.zeros(n_feature)
+        n_samples, n_features = X.shape
+
+        # init parameters
+        self.weights = np.zeros(n_features)
         self.bias = 0
 
+        # gradient descent
         for _ in range(self.n_iters):
-            y_predicted = np.dot(X, self.weight) + self.bias
+            y_predicted = np.dot(X, self.weights) + self.bias
+            # compute gradients
+            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1 / n_samples) * np.sum(y_predicted - y)
 
-            dw = (1/n_samples) * np.dot(X.T, (y_predicted - y))
-            db = (1/n_samples) * np.sum(y_predicted - y)
-
+            # update parameters
             self.weights -= self.lr * dw
-            self.bias -= self.lr * db 
+            self.bias -= self.lr * db
 
     def predict(self, X):
-        y_predicted = np.dot(X, self.weight) + self.bias
-
-        return y_predicted
+        y_approximated = np.dot(X, self.weights) + self.bias
+        return y_approximated

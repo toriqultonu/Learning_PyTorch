593b8c78bdaee087be44c705558ed9009b39dac5
toriqultonu <50527978+toriqultonu@users.noreply.github.com>
2023-07-30 19:48:24 +0600

    update

--- YOLO/yolo_test/male-female-dress.py
+++ YOLO/yolo_test/male-female-dress.py
@@ -6,5 +6,5 @@ model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
 # Use the model
 model.train(
-    data="/home/dexton/Github/Learning_PyTorch/YOLO/yolo_test/data.yaml", epochs=3
+    data="/home/dexton/Github/Learning_PyTorch/YOLO/yolo_test/data.yaml", epochs=1
 )  # train the model
